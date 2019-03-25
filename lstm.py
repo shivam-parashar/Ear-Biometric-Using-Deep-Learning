68ded691a0259c11588ee0e1b913656010be6a87
Shivam Parashar <mailtoageek@gmail.com>
2019-03-26 04:51:03 +0530

    LSTM network over extracted SIFT descriptors
    Finally using LSTM neural networks to classify ear images through extracted ORB features descriptors

--- lstm.py
+++ lstm.py
@@ -1,10 +1,8 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-from time import time
 import glob
 import tensorflow as tf
-from keras.callbacks import TensorBoard
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -16,12 +14,10 @@ from keras.layers.normalization import BatchNormalization
 from keras.utils import np_utils
 from scipy import stats
 from keras import backend as K
-from keras.callbacks import Callback
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
-import matplotlib.pyplot as pltra
-import keras_metrics
-
+import matplotlib.pyplot as plt
 K.set_image_dim_ordering('th')
+
+
 
 def normalize(X):
 	for j in xrange(0,X.shape[1]):
@@ -38,19 +34,16 @@ def normalize(X):
 #sift = cv2.SIFT()
 sift = cv2.xfeatures2d.SIFT_create()
 
-k_max =150
+k_max =300
 
-filelist = glob.glob('etrain/*.jpg')
+filelist = glob.glob('atrain/*.JPG')
 
 X_train = []
 y_train = []
 for f1 in filelist:
 	print(f1)
 	img = cv2.imread(f1,0)
-	alg = cv2.KAZE_create()
-	kps = alg.detect(img)
-	kps = sorted(kps, key=lambda x: -x.response)[:150]
-	kps, des = alg.compute(img, kps)
+	Kp, des = sift.detectAndCompute(img,None)
 	Des = np.zeros((k_max,des.shape[1]))
 	Des[:des.shape[0],:des.shape[1]] = des
 	X_train.append(Des)
@@ -61,7 +54,7 @@ for f1 in filelist:
 X_train = np.array(X_train).astype('float32')
 y_train = np_utils.to_categorical(y_train).astype('int32')
 
-filelist = glob.glob('etest/*.jpg')
+filelist = glob.glob('atest/*.JPG')
 
 X_test = []
 y_test = []
@@ -69,10 +62,7 @@ y_test = []
 for f1 in filelist:
 	img = cv2.imread(f1,0)
 	print(f1)
-	alg = cv2.KAZE_create()
-	kps = alg.detect(img)
-	kps = sorted(kps, key=lambda x: -x.response)[:150]
-	kps, des = alg.compute(img, kps)
+	Kp, des = sift.detectAndCompute(img,None)
 	Des = np.zeros((k_max,des.shape[1]))
 	Des[:des.shape[0],:des.shape[1]] = des
 	X_test.append(Des)
@@ -84,8 +74,8 @@ y_test = np_utils.to_categorical(y_test).astype('int32')
 X_train = normalize(X_train)
 X_test = normalize(X_test)
 
-X_train = X_train.reshape(X_train.shape[0],k_max,64)
-X_test = X_test.reshape(X_test.shape[0],k_max,64)
+X_train = X_train.reshape(X_train.shape[0],k_max,128)
+X_test = X_test.reshape(X_test.shape[0],k_max,128)
 
 print(y_test)
 
@@ -95,41 +85,36 @@ def lstm_model(time_steps,hidden_size,no_tags):
     model=Sequential()
     model.add(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=True, kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid'))
     model.add(Dropout(0.2))
-	#model.add(Bidirectional(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=True, kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid')))
-	#model.add(Dropout(0.2))
-    
-    #model.add(Bidirectional(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=True, 				   kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid')))
+    model.add(Bidirectional(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=True, kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid')))
+    model.add(Dropout(0.2))
+    #model.add(Bidirectional(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=True, kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid')))
     #model.add(Dropout(0.2))
-    
     model.add(Bidirectional(LSTM(units=hidden_size,input_shape=(time_steps,hidden_size),return_sequences=False, kernel_initializer="glorot_normal", recurrent_initializer="glorot_normal", activation='sigmoid')))
     model.add(Dropout(0.2))
     model.add(Dense(no_tags))
     model.add(Activation('softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-   # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', keras_metrics.precision(label=class_value), keras_metrics.recall(label=class_value), keras_metrics.f1_score(label=class_value)])
-
     return model
 
 
-
-
-model = lstm_model(k_max,64,y_train.shape[1])
+model = lstm_model(k_max,128,y_train.shape[1])
 print(model.summary())
 
-tbCallBack = TensorBoard(log_dir='data/{}'.format(time()))
 
-history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=60, batch_size=1, callbacks=[tbCallBack])
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=1)
 
 #Final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy of LSTM is: %.2f%%" % (scores[1]*100))
 print(y_test)
 
-
+precision = history.history['val_precision'][0]
+recall = history.history['val_recall'][0]
+f_score = (2.0 * precision * recall) / (precision + recall)
+print 'F1-SCORE {}'.format(f_score)
 
 print(history.history.keys())
 # summarize history for accuracy
-
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('model accuracy')
@@ -143,53 +128,10 @@ plt.plot(history.history['val_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left') 
-plt.show()
-
-'''
-plt.plot(history.history['precision'])
-plt.plot(history.history['recall'])
-plt.title('precision recall curve')
-plt.ylabel('precision')
-plt.xlabel('epoch')
-plt.legend(['precision', 'recall'], loc='upper left') 
+plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 
 
-plt.plot(history.history['val_precision'])
-plt.plot(history.history['val_recall'])
-plt.title('precision recall curve')
-plt.ylabel('precision')
-plt.xlabel('epoch')
-plt.legend(['val precision', 'val recall'], loc='upper left') 
-plt.show()
-
-plt.plot(history.history['val_precision'])
-plt.plot(history.history['val_recall'])
-plt.title('precision recall curve')
-plt.ylabel('precision')
-plt.xlabel('epoch')
-plt.legend(['val precision', 'val recall'], loc='upper left') 
-plt.show()
-
-
-plt.plot(history.history['precision'])
-plt.plot(history.history['recall'])
-plt.plot(history.history['val_precision'])
-plt.plot(history.history['val_recall'])
-plt.title('precision recall curve')
-plt.ylabel('precision')
-plt.xlabel('epoch')
-plt.legend(['precision', 'recall', 'val precision', 'val recall'], loc='upper left') 
-plt.show()'''
 
 
 
-
-# serialize model to YAML
-model_yaml = model.to_yaml()
-with open("model.yaml", "w") as yaml_file:
-    yaml_file.write(model_yaml)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
